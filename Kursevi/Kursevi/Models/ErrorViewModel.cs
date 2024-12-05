ac469c5cbc2c74fa8af48a163bd3e399706efe07
Marko <bijelicmarko311@gmail.com>
2024-12-05 20:02:59 +0100

    Dodat je asp.net

---
+++ Kursevi/Kursevi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kursevi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
