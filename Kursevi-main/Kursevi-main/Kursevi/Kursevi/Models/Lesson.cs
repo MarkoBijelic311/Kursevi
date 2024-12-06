5b46ffc855deceb5a07bcb8a87c0ac6ba3063580
Marko <bijelicmarko311@gmail.com>
2024-12-07 00:58:48 +0100

    Uradjena baza podataka putem code-first pristupa.

---
+++ Kursevi-main/Kursevi-main/Kursevi/Kursevi/Models/Lesson.cs
@@ -0,0 +1,12 @@
+﻿namespace Kursevi.Models
+{
+    public class Lesson
+    {
+        public int id_lesson { get; set; }
+        public string Content { get; set; }
+        public string LessonTitle {  get; set; }
+        public int ModuleId { get; set; }
+        public Module Module { get; set; }
+        public ICollection<LessonProgress> Progress { get; set; }
+    }
+}
---
+++ Kursevi-main/Kursevi-main/Kursevi/Kursevi/Models/StudentCourse.cs
@@ -0,0 +1,10 @@
+﻿namespace Kursevi.Models
+{
+    public class StudentCourse
+    {
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public Users User { get; set; } 
+        public Course Course { get; set; }
+    }
+}
