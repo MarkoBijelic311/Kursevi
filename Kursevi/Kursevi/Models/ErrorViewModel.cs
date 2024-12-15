22c7265eb4a02ab3748619794d411c6c08d4413d
MarkoBijelic311 <120690982+MarkoBijelic311@users.noreply.github.com>
2024-12-15 18:33:24 +0100

    Delete Kursevi directory

--- Kursevi/Kursevi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Kursevi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
